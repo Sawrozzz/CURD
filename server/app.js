54b0f7e6856fed583d612da6076c42acbfaf5fb9
[saroj adhikari] <sawroz.adhikari@gmail.com>
2024-05-19 19:02:56 +0545

    refactor

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
+const http = require('http')
+const routes  = require("./routes/routes");
 const express = require("express");
-const userModel = require("./models/user");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+
 const cookie = require("cookie-parser");
-const authenticate = require("./middleware/auth");
 
 app.use(express.json());
 
@@ -14,125 +13,19 @@ app.use(cookie());
 const jwtSecret = process.env.JWT_SECRET;
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+  cors()
 );
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("hello");
+
+
+const port = 3000;
+const hostname = "localhost";
+app.use("/", routes);
+app.use(express.json());
+const server = http.createServer(app);
+
+server.listen(port, async(req, res) => {
+  console.log(`server started at http://}`);
 });
-
-app.get("/users", (req, res) => {
-
-  userModel
-    .find({})
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
-});
-app.get("/users/getProfile/:id", authenticate, (req, res) => {
-  const id = req.params.id;
-  userModel
-    .findById({ _id: id })
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
-});
-
-app.get("/users/getUser/:id", authenticate, (req, res) => {
-  const id = req.params.id;
-  userModel
-    .findById({ _id: id })
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
-});
-
-app.put("/users/updateUser/:id",authenticate, (req, res) => {
-  const id = req.params.id;
-  userModel
-    .findByIdAndUpdate(
-      { _id: id },
-      { name: req.body.name, email: req.body.email }
-    )
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
-});
-
-app.delete("/users/deleteUser/:id", authenticate, (req, res) => {
-  const id = req.params.id;
-  userModel
-    .findByIdAndDelete(
-      { _id: id },
-      { name: req.body.name, email: req.body.email }
-    )
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
-});
-
-
-
-app.post("/create", async (req, res) => {
-  let { name, email, password } = req.body;
-  try {
-    let user = await userModel.findOne({ email });
-    if (user) return res.status(500).json({ error: "User already exists" });
-    const hashPassword = await bcrypt.hash(password, 10)
-    const createdUser = await userModel.create({
-      email,
-      name, password:hashPassword
-    })
-    res.status(200).send(JSON.stringify(createdUser))
-   
-    return;
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).json({ error: "Server Error" });
-  }
-});
-app.post("/register", async (req, res) => {
-  let { name, email, password } = req.body;
-  try {
-    let user = await userModel.findOne({ email });
-    if (user) return res.status(500).json({ error: "User already exists" });
-    const hashPassword = await bcrypt.hash(password, 10);
-    const createdUser = await userModel.create({
-      email,
-      name,
-      password: hashPassword,
-    });
-    res.status(200).send(JSON.stringify(createdUser));
-
-    return;
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).json({ error: "Server Error" });
-  }
-});
-
-app.post("/login", async (req, res) => {
-  let { email, password } = req.body;
-
-  try {
-    let user = await userModel.findOne({ email });
-    if (!user) return res.status(400).json({ error: "Invalid credentials" });
-
-    bcrypt.compare(password, user.password, function (err, result) {
-      if (result) {
-        let token = jwt.sign({ email: email, userid: user._id }, jwtSecret);
-
-    
-        res.status(200).json({ token });
-      } else {
-        res.status(400).json({ error: "Invalid credentials" });
-      }
-    });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).json({ error: "Server Error" });
-  }
-});
-
-
-app.listen(3000);
---
+++ server/routes/routes.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie-parser");
+const authenticate = require('../middleware/auth')
+// const { model } = require("mongoose");
+const router = express.Router()
+app.use(express.json());
+const userModel = require('../models/user')
+
+app.use(cookie());
+const jwtSecret = process.env.JWT_SECRET;
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+
+router.get("/", (req, res) => {
+  res.send("hello");
+});
+
+router.get("/users",authenticate, (req, res) => {
+  userModel
+    .find({})
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+router.get("/users/getProfile/:id", authenticate, (req, res) => {
+  const id = req.params.id;
+  userModel
+    .findById({ _id: id })
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+router.get("/users/getUser/:id", authenticate, (req, res) => {
+  const id = req.params.id;
+  userModel
+    .findById({ _id: id })
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+router.put("/users/updateUser/:id", authenticate, (req, res) => {
+  const id = req.params.id;
+  userModel
+    .findByIdAndUpdate(
+      { _id: id },
+      { name: req.body.name, email: req.body.email }
+    )
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+router.delete("/users/deleteUser/:id", authenticate, (req, res) => {
+  const id = req.params.id;
+  userModel
+    .findByIdAndDelete(
+      { _id: id },
+      { name: req.body.name, email: req.body.email }
+    )
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+router.post("/create", async (req, res) => {
+  let { name, email, password } = req.body;
+  try {
+    let user = await userModel.findOne({ email });
+    if (user) return res.status(500).json({ error: "User already exists" });
+    const hashPassword = await bcrypt.hash(password, 10);
+    const createdUser = await userModel.create({
+      email,
+      name,
+      password: hashPassword,
+    });
+    res.status(200).send(JSON.stringify(createdUser));
+
+    return;
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+router.post("/register", async (req, res) => {
+  let { name, email, password } = req.body;
+  try {
+    let user = await userModel.findOne({ email });
+    if (user) return res.status(500).json({ error: "User already exists" });
+    const hashPassword = await bcrypt.hash(password, 10);
+    const createdUser = await userModel.create({
+      email,
+      name,
+      password: hashPassword,
+    });
+    res.status(200).send(JSON.stringify(createdUser));
+
+    return;
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+
+  try {
+    let user = await userModel.findOne({ email });
+    if (!user) return res.status(400).json({ error: "Invalid credentials" });
+
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (result) {
+        let token = jwt.sign({ email: email, userid: user._id }, jwtSecret);
+
+        res.status(200).json({ token });
+      } else {
+        res.status(400).json({ error: "Invalid credentials" });
+      }
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+module.exports = router;
